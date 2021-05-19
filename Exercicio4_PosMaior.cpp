5418872f360bf2356cbf6eb4845183c15d0cd6aa
Erica Silva Pinto <81305764+ericasilva591@users.noreply.github.com>
2021-05-19 16:14:41 -0300

    Update Exercicio4_PosMaior.cpp

--- Exercicio4_PosMaior.cpp
+++ Exercicio4_PosMaior.cpp
@@ -13,8 +13,9 @@ void print_vector(std::vector<T>& v) {//função para imprimir um vetor
 }
 
 
+//----------------------- Max por Divide and Conquer-----------------------
 template <typename T>
-void posmaior(std::vector<T>& v, int l, int r, int& max_index) {
+void DCmax(std::vector<T>& v, int l, int r, int& max_index) {
     if (l == r) {
         max_index = l;
     }
@@ -29,8 +30,8 @@ void posmaior(std::vector<T>& v, int l, int r, int& max_index) {
     else if (r - l > 1) {
         int max2 = 0;
 
-        posmaior(v, l, std::floor((r + l) / 2), max_index);
-        posmaior(v, std::floor((r + l) / 2) + 1, r, max2);
+        DCmax(v, l, std::floor((r + l) / 2), max_index);
+        DCmax(v, std::floor((r + l) / 2) + 1, r, max2);
 
         max_index = (v[max_index] < v[max2] ? max2 : max_index);
 
@@ -38,20 +39,49 @@ void posmaior(std::vector<T>& v, int l, int r, int& max_index) {
 
 }
 
+
+//----------------------- Max por Brute Force-----------------------
+template <typename T>
+void BFmax(std::vector<T>& v, int& max_index) {
+    T max = v[0];
+
+    if (v.size() == 1) {
+        max_index = 0;
+    }
+    else {
+        for (int i = 1; i < v.size(); i++) {
+            if (v[i] > max) {
+                max = v[i];
+                max_index = i;
+            }
+
+        }
+    }
+
+}
+
+
 int main()
 {
     //Exemplo com entrada de inteiros:
-    std::vector<int> vector_in = { 15,7,55,23,24,10,11,8,76,19,50, 7 };
-    int max_index = 0;
+    std::vector<int> vector_in = { 15,79,55,23,24,100,11,8,76,19,50, 7 };
+    int max_indexDC = 0;
+    int max_indexBF = 0;
 
     std::cout << "Vetor de entrada: ";
     print_vector(vector_in);
-    posmaior(vector_in, 0, vector_in.size() - 1, max_index);
-    std::cout << " Indice do maior elemento eh:" << max_index;
 
+    std::cout << std::endl;
+
+    DCmax(vector_in, 0, vector_in.size() - 1, max_indexDC);
+    std::cout << " Indice do maior elemento por DC eh:" << max_indexDC;
+
+    std::cout << std::endl;
+
+    BFmax(vector_in, max_indexBF);
+    std::cout << " Indice do maior elemento por BF eh:" << max_indexBF;
    
 }
-
 // Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
 // Depurar programa: F5 ou menu Depurar > Iniciar Depuração
 
